414fcb4aae8da9afa23ee5c807989507c1fe60da
Fenil-Gajjar <fenilgajjar.devops@gmail.com>
2024-09-06 16:43:52 +0530

    quotes in Bash Scripting

---
+++ Bash-Scripting/quotes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# in this example, backslash will ommit the special meaning of $ and will print $9 as it is.
+
+VIRUS="Covid19"
+
+echo "Due to $VIRUS virus company have lost /$9 million"
